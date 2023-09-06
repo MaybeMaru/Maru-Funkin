7224a16e044e469ca640e8968331d08018abd8ef
MaybeMaru <97055307+MaybeMaru@users.noreply.github.com>
2023-09-06 17:10:36 +0200

    improving mod menu a lil bit

--- source/funkin/states/options/items/ModItem.hx
+++ source/funkin/states/options/items/ModItem.hx
@@ -2,38 +2,40 @@ package funkin.states.options.items;
 
 class ModItem extends FlxSpriteGroup {
     public var modName:String;
-    public var modID:Int;
     public var modEnabled:Bool = true;
     
-    public var enableButton:FlxSprite;
+    public var enableButton:FlxSpriteExt;
     public var targetY:Float;
 
-    public function new(modName:String, modID:Int = 0):Void {
+    public function new(modName:String):Void {
         super();
         this.modName = modName;
-        this.modID = modID;
 
         var modBox:FlxSprite = new FlxSprite().makeGraphic(Std.int(FlxG.width / 1.5), Std.int(FlxG.height / 4), FlxColor.BLACK);
         modBox.alpha = 0.6;
         add(modBox);
 
+        var aa = Preferences.getPref('antialiasing');
+
         var icon = Paths.file('$modName/icon.png', IMAGE);
-        var modIcon:FlxSprite = new FlxSprite().loadGraphic(Paths.exists(icon, IMAGE) ? Paths.getImage(icon) : Paths.image('options/blankMod'));
-        modIcon.x += (modIcon.width/8)/2;   modIcon.y += (modIcon.height/8)/2;
-        modIcon.antialiasing = true;
-        modIcon.scale.set(0.6,0.6);
-        modIcon.updateHitbox();
+        var modIcon:FlxSpriteExt = new FlxSpriteExt();
+        modIcon.loadGraphic(Paths.exists(icon, IMAGE) ? Paths.getImage(icon) : Paths.image('options/blankMod'));
+        modIcon.antialiasing = aa;
+        modIcon.setScale(0.6);
+        modIcon.setPosition(15, modBox.height * 0.5 - modIcon.height * 0.5);
         add(modIcon);
 
-        var modTitle:Alphabet = new Alphabet(modIcon.x + modIcon.width + 10, 10, modName, true, Std.int(modBox.width/2.5), 0.8);
+        var modTitle:Alphabet = new Alphabet(modIcon.x + modIcon.width + 10, 10, modName, true, Std.int(modBox.width/2), 0.666); // SATAN
         add(modTitle);
 
         var infoText:String = CoolUtil.getFileContent(Paths.file('$modName/info.txt'));
-        var modInfo:Alphabet = new Alphabet(modTitle.x, modTitle.y + modTitle.height, infoText, false, Std.int(modBox.width/2.5*1.4), 0.6);
+        var modInfo:FlxText = new FlxText(modTitle.x, modTitle.y + modTitle.height + 5, Std.int(modBox.width*0.6), infoText);
+        modInfo.setFormat(Paths.font('phantommuff_'), 20, FlxColor.BLACK, LEFT, OUTLINE, FlxColor.WHITE);
+        modInfo.borderSize = 1.333;
         add(modInfo);
 
-        enableButton = new FlxSprite(modBox.width,modBox.height).loadGraphic(Paths.image('options/modButton'), true, 60, 58);
-        enableButton.antialiasing = Preferences.getPref('antialiasing');
+        enableButton = new FlxSpriteExt(modBox.width,modBox.height).loadImageAnimated('options/modButton', 60, 58);
+        enableButton.antialiasing = aa;
         enableButton.animation.add('on', [0]);
         enableButton.animation.add('off', [1]);
         enableButton.x -= enableButton.width + 5;
@@ -62,7 +64,10 @@ class ModItem extends FlxSpriteGroup {
         if (FlxG.mouse.justPressed && FlxG.mouse.overlaps(enableButton)) {
             clickEnable();
         }
-        enableButton.scale.x = CoolUtil.coolLerp(enableButton.scale.x, 1, 0.2);
-        enableButton.scale.y = CoolUtil.coolLerp(enableButton.scale.y, 1, 0.2);
+
+        if (enableButton.scale.x > 1) {
+            enableButton.scale.x = CoolUtil.coolLerp(enableButton.scale.x, 1, 0.2);
+            enableButton.scale.y = CoolUtil.coolLerp(enableButton.scale.y, 1, 0.2);
+        }
     }
 }
