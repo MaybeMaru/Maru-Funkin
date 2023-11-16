7a1ec845438d17417a15f71128dae07ac4eafbda
Unholywanderer <96925902+Unholywanderer@users.noreply.github.com>
2023-11-16 14:21:52 -0800

    no way auto cache
    it probably works.... maybe....

--- assets/preload/data/events/changeChar.hx
+++ assets/preload/data/events/changeChar.hx
@@ -3,3 +3,25 @@ function eventHit(event) {
         State.switchChar(event.values[0], event.values[1]);
     }
 }
+
+var cached = [];
+var already = false;
+function createPost(){
+    for(char in State.SONG.players)
+        if(char != null) cached.push(char);
+
+    for(event in State.notesGroup.events){
+        if(event.name == 'changeChar'){
+            already = false;
+            for(cachedChar in cached){
+                if(cachedChar == event.values[1]){
+                    already = true;
+                    break; 
+                }
+            }
+            if(already) continue; // they already cached fuck off to the next one!!!!!
+            cached.push(event.values[1]);
+            cacheCharacter(event.values[1]);
+        }
+    }
+}
