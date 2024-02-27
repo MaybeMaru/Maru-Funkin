e064de80f5cad99df40c2f69c01290dbbb841d2c
Unholywanderer <96925902+Unholywanderer@users.noreply.github.com>
2024-02-27 14:51:59 -0800

    add cam zoom like in psyched engine.... woah.... (#29)
    * add cam zoom like in psyched engine.... woah....
    
    * Update events-sort.txt
    
    maruru stinkss....
    
    * femboys really arent that bad bro that femboyussy got me actin up
    
    * Update addZoom.json
    
    ---------
    
    Co-authored-by: MaybeMaru <97055307+MaybeMaru@users.noreply.github.com>

---
+++ assets/preload/data/events/addZoom.hx
@@ -0,0 +1,10 @@
+function eventHit(event) { // im SICK of making a new 'Add Camera Zoom'... 
+    if (event.name == 'addZoom' && getPref('camera-zoom')) {
+        var vals = event.values;
+        if (Math.isNaN(Std.parseFloat(vals[0]))) vals[0] = 0.015;   
+        if (Math.isNaN(Std.parseFloat(vals[1]))) vals[1] = 0.03;   
+
+        State.camGame.zoom += Std.parseFloat(vals[0]);
+        State.camHUD.zoom += Std.parseFloat(vals[1]);
+    }
+}
